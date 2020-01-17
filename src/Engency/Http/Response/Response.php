433ea615ddfd8e1fb99c0aebd7e05990f871722d
Frank Kuipers <frankkuipers@outlook.com>
2020-01-17 11:30:24 +0100

    Fix

--- src/Engency/Http/Response/Response.php
+++ src/Engency/Http/Response/Response.php
@@ -165,8 +165,8 @@ class Response implements Responsable
         return [
             'meta'       => $this->responseMeta,
             'data'       => array_merge($this->data, $this->getInstanceData()),
-            'serverTime' => time(),
-            'success'    => (bool) $this->success,
+            'ServerTime' => time(),
+            'Success'    => (bool) $this->success,
         ];
     }
 
